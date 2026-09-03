bc27cfcc98fb4f7c0066a907c22324540897041c
Vijaya <vijaya.salveru@wnco.com>
2026-09-03 14:34:45 +0530

    execute

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -11,5 +11,3 @@ test('successful login', async ({ page }) => {
   await expect(page).toHaveURL(/secure/);
   await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
 });
-
-
